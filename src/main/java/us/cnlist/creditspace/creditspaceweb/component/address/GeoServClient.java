9b32f4c02c4eb1b436bfe0db6b6f6bdae3dee951
Sergei W. Adonev <admin@cnlist.us>
2020-01-31 16:36:14 +0900

    LAV-12 create addresses form

---
+++ src/main/java/us/cnlist/creditspace/creditspaceweb/component/address/GeoServClient.java
@@ -0,0 +1,12 @@
+package us.cnlist.creditspace.creditspaceweb.component.address;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GeoServClient {
+
+    private RestTemplate restTemplate;
+
+
+}
